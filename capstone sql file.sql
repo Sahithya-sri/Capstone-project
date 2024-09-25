5610fc236ee9af2063788aa594ca5bb777dc2550
Sahithya-sri <sahithyasri200208@gmail.com>
2024-09-25 08:23:24 +0530

    Add files via upload

---
+++ capstone sql file.sql
@@ -0,0 +1,8 @@
+use football;
+show Tables;
+select * from appearances;
+select * from game_events;
+select * from game_lineups;
+select * from games;
+select * from players;
+select * from football_merged_file;
